df64cb8a7afbaf5b433b59d95823ab43020462f8
Brian Washington <bw49078@gmail.com>
2022-10-06 14:58:48 -0500

    finished

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -11,6 +11,13 @@ arrowRestSum(0); // => 0
 ***********************************************************************/
 
 // Your code here
+let arrowRestSum = (...nums) => {
+  return nums.reduce((accum, next) => accum += next);
+}
+
+console.log(arrowRestSum(3, 5, 6)); // => 14
+arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
+arrowRestSum(0); // => 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
